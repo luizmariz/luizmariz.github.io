97ae793573a3a3bc47ed5e7994c10cc92820eefc
Luiz Gustavo Oliveira Mariz <luiz.mariz@MJV.net>
2019-08-30 19:22:06 -0300

    added: all sort of plugins ❤️

---
+++ gatsby-config.js
@@ -0,0 +1,85 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'http://localhost:8000'
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          'gatsby-remark-autolink-headers',
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              inlineCodeMarker: '÷',
+            },
+          },
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'noopener'
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Luiz Ipsum',
+        short_name: 'LuizIpsum',
+        start_url: '/',
+        background_color: '#FFFFFF',
+        theme_color: '#59C9A5',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/logo_512px.png'
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-146759812-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/']
+      }
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `darkorange`,
+        showSpinner: false
+      }
+    }
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+import React from "react"
+
+export default () => <div>Behold my site's fury!</div>
