0dc2bf26e9c6fe3e9041048708d7bab57214e2bc
Luiz Gustavo Oliveira Mariz <luiz.mariz@MJV.net>
2019-10-06 16:34:11 -0300

    added: last about me page details

---
+++ src/components/shared/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const SubTitle = styled.span`
+  text-transform: uppercase;
+  font-family: 'Fira Code Bold';
+  color: ${props => props.theme.secondary};
+`;
