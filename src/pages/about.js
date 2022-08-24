8bae7e3b8803bb781c60e22679cddb5a2fe3ae2a
Luiz Gustavo Oliveira <37978726+luizmariz@users.noreply.github.com>
2022-08-24 14:04:19 -0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,20 +24,19 @@ export default function({ location }) {
       <article>
         <p>
           Oi, meu nome é Luiz, nasci no interior de Minas Gerais e atualmente
-          sou dev no{' '}
+          sou dev na{' '}
           <a
-            href="https://www.fab.mil.br/organizacoes/mostra/67/CENTRO%20DE%20COMPUTA%C3%87%C3%83O%20DE%20AERON%C3%81UTICA%20DO%20RIO%20DE%20JANEIRO"
+            href="https://www.totvs.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            CCA-RJ
+            TOTVS
           </a>
-          , no Rio de Janeiro/RJ, além de ser um estudante em ciência da
-          computação na UFRJ e técnico em eletrônica pelo CEFET/RJ.
+          , no Rio de Janeiro/RJ :)
         </p>
         <p>
-          Apesar de vender meu peixe como dev react, lido bastante com backend,
-          me aventurei em projetos de IoT e em outras perólas da programação. No
+          Apesar de vender meu peixe como dev front-end, já lidei bastante com backend,
+          me aventurei em projetos de IoT e em outras áreas da programação. No
           fim, não importa a linguagem, tecnologia ou paradigma, a sensação mais
           legal é ver alguma coisa que você fez funcionando linda e maravilhosa.
           E, no geral, aprendo bem.
